11f68881b48737904fa2fe5a6a2c98b942b50bd1
Max <61598918+Intestabilis@users.noreply.github.com>
2024-04-09 05:12:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,77 @@
-import os
-import random
 import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.all()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
 
-client = discord.Bot()
-token = os.getenv('MTIyNzA2MjQ1NzQ3NDc0NDM5Mw.GCtvuh.ExcTzhQM2tsWjb_Gv78e2uvebTrOlwPQN_NtOc')
-
-@client.event
+@bot.event
 async def on_ready():
-    print("Logged in as a bot {0.user}".format(client))
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split("#")[0]
-    channel = str(message.channel.name)
-    user_message = str(message.content)
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 
-    print(f'Message {user_message} by {username} on {channel}')
 
-    if message.author == client.user:
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
         return
 
-    if channel == "random":
-        if user_message.lower() == "hello" or user_message.lower() == "hi":
-            await message.channel.send(f'Hello {username}')
-            return
-        elif user_message.lower() == "bye":
-            await message.channel.send(f'Bye {username}')
-        elif user_message.lower() == "tell me a joke":
-            jokes = [" Can someone please shed more\
-                     light on how my lamp got stolen?",
-                     "Why is she called llene? She\
-            stands on equal legs.",
-                     "What do you call a gazelle in a \
-            lions territory? Denzel."]
-            await message.channel.send(random.choice(jokes))
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
 
-client.run(token)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+
+bot.run('MTIyNzA2MjQ1NzQ3NDc0NDM5Mw.GCtvuh.ExcTzhQM2tsWjb_Gv78e2uvebTrOlwPQN_NtOc')
