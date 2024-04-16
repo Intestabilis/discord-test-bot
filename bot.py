a1295c9b722c9c3f5cab48b2c66b7c50fa11434b
Max <61598918+Intestabilis@users.noreply.github.com>
2024-04-16 14:31:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ async def on_ready():
 
 @bot.command()
 async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
+    "Adds two numbers together."
     await ctx.send(left + right)
 
 
